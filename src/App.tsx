33ed19f85c9597e0057e311fb26424a84aee6063
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-06-29 22:28:26 +0200

    added some css and comp login

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import Header from './components/header/Header'
-import Menu from './components/Menu/Menu'
-import { Routes, Route, Link } from 'react-router-dom'
-import Home from './components/homeScreen/Home'
-import Login from './components/login/Login'
-
-
+import { useState } from "react";
+import Header from "./components/header/Header";
+import Menu from "./components/Menu/Menu";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/homeScreen/Home";
+import Login from "./components/login/Login";
 
 function App() {
-  const[toogleMenu, setToogleMenu] = useState(false)
+  const [toogleMenu, setToogleMenu] = useState(false);
   const openCloseMenu = () => {
-    setToogleMenu(prevToogleMenu=> !prevToogleMenu)
-    console.log(toogleMenu)
-  }
-  const closeMenu=()=>{
-    setToogleMenu(true)
-  }
-
+    setToogleMenu((prevToogleMenu) => !prevToogleMenu);
+    console.log(toogleMenu);
+  };
+  const closeMenu = () => {
+    setToogleMenu(true);
+  };
 
   return (
-    <div className="App">
-      <Header title="ArtyFy" titleClick={closeMenu} fn={openCloseMenu}/>
+    <div className='App'>
+      <Header title='ArtyFy' titleClick={closeMenu} fn={openCloseMenu} />
       <Menu toogleMenu={toogleMenu} closeMenu={closeMenu} />
       <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/login' element={ <Login/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
-    
-   
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,9 +1,23 @@
-import Styles from './login.module.css'
+import Styles from "./login.module.css";
 
-const Login = () => {
+const Login: React.FC = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("click");
+  };
   return (
-    <div>Login</div>
-  )
-}
+    <div className={Styles.container}>
+      <h2 className={Styles.title}>Login</h2>
+      <form className={Styles.form}>
+        <label htmlFor=''>Username:</label>
+        <input type='text' />
+        <label htmlFor=''>Password:</label>
+        <input type='password' />
+        <p className={Styles.createAccount}>Create account ?</p>
+        <button onClick={(e) => handleClick(e)}>Login</button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
