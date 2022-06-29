365eec880eaa091ad6d2aefd80ad5f7af00bc341
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-06-29 16:52:36 +0200

    menu css

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Styles from './menu.module.css'
 
 
@@ -7,7 +8,12 @@ interface Props{
 
 const Menu: React.FC<Props> = ({toogleMenu}) => {
   return (
-    <nav className={toogleMenu? Styles.container:Styles.containerOpen}></nav>
+    <nav className={toogleMenu? Styles.container:Styles.containerOpen}>
+      <Link className={ toogleMenu? Styles.link: Styles.linkOpen} to={'/login'}>Login</Link>
+      <Link className={ toogleMenu? Styles.link: Styles.linkOpen} to={'/login'}>About</Link>
+      <Link className={ toogleMenu? Styles.link: Styles.linkOpen} to={'/login'}>Contact</Link>
+      <Link className={ toogleMenu? Styles.link: Styles.linkOpen} to={'/login'}>Home</Link>
+    </nav>
   )
 }
 
