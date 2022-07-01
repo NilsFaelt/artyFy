55fc70ca767c96797d477f0bae0e917ec1489ae0
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-07-01 10:22:43 +0200

    background

---
+++ src/users.tsx
@@ -0,0 +1,8 @@
+
+    {
+      username: "Kilmajaro",
+      password: "lotus_55",
+      email: "Kilmajaro@gmail.com",
+      artImage:
+        "https://images.unsplash.com/photo-1515405295579-ba7b45403062?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
+    },
